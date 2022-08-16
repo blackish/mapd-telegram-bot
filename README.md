a56a04a6177046b805bcbfb366887f29959c65d9
Андрей Лысов <blackish@Andrejs-MacBook-Air.local>
2022-08-16 14:20:52 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Telegram bot that read status of invertor (www.intertor.ru) from shared memory and send alarms to telegram
---
+++ mapd_bot.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python3
+import logging
+import asyncio
+import datetime
+from telegram import Bot, ReplyKeyboardRemove, Message
+from telegram.error import Forbidden, NetworkError
+import sysv_ipc
+import json
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+sm = sysv_ipc.SharedMemory(2015)
+CHAT_ID = 0
+TOKEN = "0"
+
+async def parse_message(msg: Message):
+    global sm
+    if msg.text == "ping":
+        await msg.reply_text("bot is alive")
+    elif msg.text == "status":
+        data = sm.read()
+        j = json.loads(data.decode('utf-8').strip('\x00'))
+        reply = f"Unet: {j['_UNET']}\nInet: {j['_INET_16_4']}\nUout: {j['_UOUTmed']}\nPnet: {j['_PNET']}\nUacc: {j['_Uacc']}\nIacc: {j['_Iacc']}"
+        await msg.reply_text(reply)
+
+async def main():
+    global sm, CHAT_ID, TOKEN
+    mapd = False
+    Uinput = False
+    date_loss = datetime.datetime.now()
+    async with Bot(TOKEN) as bot:
+        last_update = 0
+        while True:
+            try:
+                updates = (await bot.get_updates(offset=last_update))
+                for i in updates:
+                    last_update = i.update_id+1
+                    if i.message.chat.id == CHAT_ID:
+                        await parse_message(i.message)
+            except Exception as e:
+                print(e)
+            await asyncio.sleep(5)
+            data = sm.read()
+            j = json.loads(data.decode('utf-8').strip('\x00'))
+            now = datetime.datetime.now()
+            last_check = datetime.datetime.strptime(f"{now.year}-{now.month}-{now.day} {j['time']}","%Y-%m-%d %H:%M:%S")
+            if (now-last_check).seconds > 3600 and mapd:
+                await bot.send_message(CHAT_ID, "MAPD is probably dead. Please fix")
+                mapd = False
+            if (now-last_check).seconds < 3600 and not mapd:
+                await bot.send_message(CHAT_ID, "MAPD is back to normal")
+                mapd = True
+            if j["_MODE"] != "3" and Uinput:
+                date_loss = datetime.datetime.now()
+                Uinput = False
+                await bot.send_message(CHAT_ID,f"Lost power from grid at {date_loss} UTC")
+            if j["_MODE"] == "3" and not Uinput:
+                date_loss = datetime.datetime.now()
+                await bot.send_message(CHAT_ID,f"Power from grid restored at {date_loss} UTC\nUacc: {j['_Uacc']}")
+                Uinput = True
+            if j["_MODE"] != 3 and not Uinput:
+                diff = datetime.datetime.now() - date_loss
+                if diff.seconds%600 == 0:
+                    await bot.send_message(CHAT_ID,f"No power from grid.\nUacc: {j['_Uacc']}\nIacc: {j['_IAcc']}")
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
