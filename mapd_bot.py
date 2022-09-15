fbee7997ea4021e1f3ee4637e88a07189018acfb
Андрей Лысов <blackish@Andrejs-Air.lan>
2022-09-15 20:07:08 +0300

    extend status command

--- mapd_bot.py
+++ mapd_bot.py
@@ -22,8 +22,18 @@ async def parse_message(msg: Message):
         await msg.reply_text("bot is alive")
     elif msg.text.lower() == "status":
         data = sm.read()
-        j = json.loads(data.decode('utf-8').strip('\x00'))
-        reply = f"Unet: {j['_UNET']}\nInet: {j['_INET_16_4']}\nUout: {j['_UOUTmed']}\nPnet: {j['_PNET']}\nUacc: {j['_Uacc']}\nIacc: {j['_Iacc']}"
+        raw = data.decode('utf-8').strip('\x00')
+        txt = raw.strip('{} ')
+        endstr = txt.find('_flagUnet2')-2
+        txt = "{"+txt[:endstr]+"}"
+        j = json.loads(txt)
+        if j['_MODE'] == "3":
+            reply = "Mode: power from grid\n"
+        elif j['_MODE'] == "4":
+            reply = "Mode: Charging\n"
+        else:
+            reply = "Mode: No power from grid\n"
+        reply += f"Unet: {j['_UNET']}\nInet: {j['_INET_16_4']}\nUout: {j['_UOUTmed']}\nPnet: {j['_PNET']}\nUacc: {j['_Uacc']}\nIacc: {j['_IAcc_med_A_u16']}"
         await msg.reply_text(reply)
 
 async def main():
@@ -44,7 +54,10 @@ async def main():
                 print(e)
             await asyncio.sleep(5)
             data = sm.read()
-            txt = data.decode('utf-8').strip('\x00')
+            raw = data.decode('utf-8').strip('\x00')
+            txt = raw.strip('{} ')
+            endstr = txt.find('_flagUnet2')-2
+            txt = "{"+txt[:endstr]+"}"
             try:
                 j = json.loads(txt)
                 now = datetime.datetime.now()
@@ -52,26 +65,27 @@ async def main():
             except Exception:
                 await bot.send_message(CHAT_ID,f"Failed to parse data: {txt}")
                 continue
-            now = datetime.datetime.now()
-            last_check = datetime.datetime.strptime(f"{now.year}-{now.month}-{now.day} {j['time']}","%Y-%m-%d %H:%M:%S")
-            if (now-last_check).seconds > 3600 and mapd:
-                await bot.send_message(CHAT_ID, "MAPD is probably dead. Please fix")
-                mapd = False
-            if (now-last_check).seconds < 3600 and not mapd:
-                await bot.send_message(CHAT_ID, "MAPD is back to normal")
-                mapd = True
-            if j["_MODE"] != "3" and Uinput:
-                date_loss = datetime.datetime.now()
-                Uinput = False
-                await bot.send_message(CHAT_ID,f"Lost power from grid at {date_loss} UTC")
-            if j["_MODE"] == "3" and not Uinput:
-                date_loss = datetime.datetime.now()
-                await bot.send_message(CHAT_ID,f"Power from grid restored at {date_loss} UTC\nUacc: {j['_Uacc']}")
-                Uinput = True
-            if j["_MODE"] != 3 and not Uinput:
-                diff = datetime.datetime.now() - date_loss
-                if diff.seconds%600 == 0:
-                    await bot.send_message(CHAT_ID,f"No power from grid.\nUacc: {j['_Uacc']}\nIacc: {j['_IAcc']}")
+            try:
+                if (now-last_check).seconds > 3600 and mapd:
+                    await bot.send_message(CHAT_ID, "MAPD is probably dead. Please fix")
+                    mapd = False
+                if (now-last_check).seconds < 3600 and not mapd:
+                    await bot.send_message(CHAT_ID, "MAPD is back to normal")
+                    mapd = True
+                if not j["_MODE"] in ("3","4") and Uinput:
+                    date_loss = datetime.datetime.now()
+                    Uinput = False
+                    await bot.send_message(CHAT_ID,f"Lost power from grid at {date_loss} UTC")
+                if j["_MODE"] in ("3","4") and not Uinput:
+                    date_loss = datetime.datetime.now()
+                    await bot.send_message(CHAT_ID,f"Power from grid restored at {date_loss} UTC\nUacc: {j['_Uacc']}")
+                    Uinput = True
+                if not j["_MODE"] in ("3","4") and not Uinput:
+                    diff = datetime.datetime.now() - date_loss
+                    if diff.seconds%600 == 0:
+                        await bot.send_message(CHAT_ID,f"No power from grid.\nUacc: {j['_Uacc']}\nIacc: {j['_IAcc_med_A_u16']}")
+            except Exception as e:
+                await bot.send_message(CHAT_ID,f"Exception {e}")
 
 
 if __name__ == '__main__':
